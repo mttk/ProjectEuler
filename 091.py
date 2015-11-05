b3a76585f7a9a31c7e3aa1786fa72b87fbfcd582
mtutek <=>
2015-11-05 19:43:51 +0100

    091

---
+++ 091.py
@@ -0,0 +1,13 @@
+__author__ = 'mttk'
+
+import numpy as np
+from fractions import gcd
+
+size = 50
+result = 3*size**2 ## special cases
+for a in range(1, size+1):
+    for b in range(1, size+1):
+        f = gcd(a, b)
+        result += min(b*f / a, (size - a)*f / b) * 2
+
+print result
