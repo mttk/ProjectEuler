77560c14c6005222c4c5760d4b0e1139340fb2d3
mtutek <martin.tutek@gmail.com>
2016-01-28 19:21:24 +0100

    026 comment

--- 026.py
+++ 026.py
@@ -1,23 +1,30 @@
 ### Reciprocal cycles
 # https://projecteuler.net/problem=26
 
+
 def division(n, d):
+    """
+    A cycle is defined by the number being divided and the divisor. Such a pair uniquely defines
+    the sequence of numbers occurring as a result of the division - if such a pair occurs twice consequently,
+    then the numbers between the occurrences are the elements of the cycle.
+    """
     signatures = {}
     i = 0
     while True:
         if n == 0:
             break
         while n < d:
-            n*=10
-        div = n // d
-        mod = n % d
-        if (div, mod) in signatures:
-            cycle_len = i - signatures[(div, mod)]
+            n *= 10
+
+        if n in signatures:
+            cycle_len = i - signatures[n]
             return cycle_len
         else:
-            signatures[(div, mod)] = i
-        n = mod
+            signatures[n] = i
+
+        n = n % d
         i += 1
+    return 0
 
 max_num = 0.
 max_cycle = 0.
